0b0a71c753eaa4045619cd0923fa13b807810e79
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-15 13:10:20 -0600

    Add initial Athlete FactoryBot factory

---
+++ spec/factories/athletes.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :athlete do
+    name { "MyName" }
+    sex { "GenderIsAConstruct" }
+    age { 25 }
+    height { 175 }
+    weight { 70 }
+    team { "Teamy McTeamface" }
+    games { "PlayNo" }
+    sport { "Sportball" }
+    event { "Cookout" }
+    medal { nil }
+  end
+end
