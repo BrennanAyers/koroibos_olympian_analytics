a0d8aa801bb8c5d97cd08f317d3c03b36a47e000
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-15 13:05:47 -0600

    Add initial Athlete migration with all attributes, Update DB schema

---
+++ db/migrate/20190915185548_create_athletes.rb
@@ -0,0 +1,18 @@
+class CreateAthletes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :athletes do |t|
+      t.string :name
+      t.string :sex
+      t.integer :age
+      t.integer :height
+      t.integer :weight
+      t.string :team
+      t.string :games
+      t.string :sport
+      t.string :event
+      t.string :medal
+
+      t.timestamps
+    end
+  end
+end
