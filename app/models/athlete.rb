4875c4e4fa651502b44e0fdd7ad4531ce742bbe9
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-15 13:46:41 -0600

    Remove presence validations on Athlete model for :height and :weight to fit provided data

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,3 +1,3 @@
 class Athlete < ApplicationRecord
-  validates_presence_of :name, :sex, :age, :height, :weight, :team, :games, :sport, :event
+  validates_presence_of :name, :sex, :age, :team, :games, :sport, :event
 end
