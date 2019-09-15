82e10c6fbee17189a2bd3ba1b46d1325b5959545
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-15 14:02:11 -0600

    Add frozen_string_literal: true to files, Change double quotes to single quotes on Athlete Factory, Add Athlete model TLD

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# The Athlete model is the main form of data storage in the app,
+# containing all pertinent information for each Olympian and their efforts.
 class Athlete < ApplicationRecord
   validates_presence_of :name, :sex, :age, :team, :games, :sport, :event
 end
--- spec/factories/athletes.rb
+++ spec/factories/athletes.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :athlete do
-    name { "MyName" }
-    sex { "GenderIsAConstruct" }
+    name { 'MyName' }
+    sex { 'GenderIsAConstruct' }
     age { 25 }
     height { 175 }
     weight { 70 }
-    team { "Teamy McTeamface" }
-    games { "PlayNo" }
-    sport { "Sportball" }
-    event { "Cookout" }
+    team { 'Teamy McTeamface' }
+    games { 'PlayNo' }
+    sport { 'Sportball' }
+    event { 'Cookout' }
     medal { nil }
   end
 end
