145ab6bf596a0e3ca31c75b3969382ad47353a6b
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-16 13:22:57 -0600

    Add initial refactor of OlympiansController#index for checking age param and passing into ::all_with_total_medals_won

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -5,7 +5,8 @@ module Api
     # The Olympians controller handles all RESTful routes for the Olympians table
     class OlympiansController < ApplicationController
       def index
-        render json: OlympianSerializer.new(Olympian.all_with_total_medals_won)
+        age = 'ASC' if params[:age] == 'youngest'
+        render json: OlympianSerializer.new(Olympian.all_with_total_medals_won(age))
       end
     end
   end
