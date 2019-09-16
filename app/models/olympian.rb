9b3fee1d3327889b13fc424bed6c7b9d2002b52b
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-15 19:17:44 -0600

    Add initial Olympians::all_with_total_medals_won method to take all Olympians and group them by name, team, age, sport, and Count of medals

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -4,4 +4,10 @@
 # containing all pertinent information for each thlete and their efforts.
 class Olympian < ApplicationRecord
   validates_presence_of :name, :sex, :age, :team, :games, :sport, :event
+
+  def self.all_with_total_medals_won
+    select('olympians.name, olympians.team, olympians.age, olympians.sport, COUNT (medal) AS total_medals_won')
+    .group(:name, :team, :age, :sport)
+    .order(:name)
+  end
 end
