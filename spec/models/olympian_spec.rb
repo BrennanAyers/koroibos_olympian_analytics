420baf30aa5a084aa7139b610f6eca4a382d0d0f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-15 18:56:33 -0600

    Add ::all_with_total_medals_won spec for Olympians model

--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -15,4 +15,24 @@ RSpec.describe Olympian, type: :model do
     it { should validate_presence_of :event }
     it { should_not validate_presence_of :medal }
   end
+
+  describe 'class methods' do
+    before :each do
+      create(:olympian, name: 'Brian Plantico', team: 'Wisconsin', age: 17, sport: 'Cycling', medal: 'Gold')
+      create(:olympian, name: 'Brian Plantico', team: 'Wisconsin', age: 17, sport: 'Cycling', medal: 'Silver')
+      create(:olympian, name: 'Brian Plantico', team: 'Wisconsin', age: 17, sport: 'Cycling', event: 'The Downhill')
+      create(:olympian, name: 'Logan Pile', team: 'Colorado', age: 55, sport: 'Wakeboarding', event: 'Sick Waves')
+      create(:olympian, name: 'Logan Pile', team: 'Colorado', age: 55, sport: 'Wakeboarding', event: 'Not So Sick Waves')
+    end
+
+    it 'should return collated Olympians with ::all_with_total_medals_won' do
+      expect(Olympian.all.length).to eq(5)
+      olympians = Olympian.all_with_total_medals_won
+      expect(olympians.length).to eq(2)
+      expect(olympians.first.name).to eq('Brian Plantico')
+      expect(olympians.last.name).to eq('Logan Pile')
+      expect(olympians.first.total_medals_won).to eq(2)
+      expect(olympians.last.total_medals_won).to eq(0)
+    end
+  end
 end
