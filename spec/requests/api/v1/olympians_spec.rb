523f1cb366a5142e3c5d4bc8b8aedd1646e87c72
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-15 18:58:53 -0600

    Fix Olympian API spec to have unique Olympians for basic spec, Fix accessing attributes for Olympians serialized spec

--- spec/requests/api/v1/olympians_spec.rb
+++ spec/requests/api/v1/olympians_spec.rb
@@ -4,7 +4,9 @@ require 'rails_helper'
 
 describe 'Olympian API' do
   it 'sends a list of olympians' do
-    create_list(:olympian, 3)
+    create(:olympian, name: 'Brian Plantico', team: 'Wisconsin', age: 17, sport: 'Cycling', medal: 'Gold')
+    create(:olympian, name: 'Earl Stephens', team: 'Florida', age: 34, sport: 'Boxing', event: 'VS Mike Tyson')
+    create(:olympian, name: 'Logan Pile', team: 'Colorado', age: 55, sport: 'Wakeboarding', event: 'Sick Waves')
 
     get '/api/v1/olympians'
 
@@ -26,16 +28,16 @@ describe 'Olympian API' do
 
     olympians = JSON.parse(response.body)['data']
 
-    expect(olympians.count).to eq(2)
-    expect(olympians.first.name).to eq('Brian Plantico')
-    expect(olympians.first.team).to eq('Wisconsin')
-    expect(olympians.first.age).to eq(17)
-    expect(olympians.first.sport).to eq('Cycling')
-    expect(olympians.first.total_medals_won).to eq(1)
-    expect(olympians.last.name).to eq('Logan Pile')
-    expect(olympians.last.team).to eq('Colorado')
-    expect(olympians.last.age).to eq(55)
-    expect(olympians.last.sport).to eq('Wakeboarding')
-    expect(olympians.last.total_medals_won).to eq(0)
+    expect(olympians.length).to eq(2)
+    expect(olympians.first['attributes']['name']).to eq('Brian Plantico')
+    expect(olympians.first['attributes']['team']).to eq('Wisconsin')
+    expect(olympians.first['attributes']['age']).to eq(17)
+    expect(olympians.first['attributes']['sport']).to eq('Cycling')
+    expect(olympians.first['attributes']['total_medals_won']).to eq(1)
+    expect(olympians.last['attributes']['name']).to eq('Logan Pile')
+    expect(olympians.last['attributes']['team']).to eq('Colorado')
+    expect(olympians.last['attributes']['age']).to eq(55)
+    expect(olympians.last['attributes']['sport']).to eq('Wakeboarding')
+    expect(olympians.last['attributes']['total_medals_won']).to eq(0)
   end
 end
