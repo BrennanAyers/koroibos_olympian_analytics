9486d9867dd1f3e6812b1a4150b7bb9db301e613
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-17 11:45:36 -0600

    Change double quotes to single quotes in OlympianStats spec

--- spec/requests/api/v1/olympian_stats_spec.rb
+++ spec/requests/api/v1/olympian_stats_spec.rb
@@ -11,7 +11,7 @@ describe 'Olympian Stats` API' do
     create(:olympian, name: 'Alexandra Chakres', age: 55, weight: 55, sex: 'F')
     create(:olympian, name: 'Earl Stephens', age: 34, weight: 70, sex: 'M')
 
-    get "/api/v1/olympian_stats"
+    get '/api/v1/olympian_stats'
 
     expect(response).to be_successful
 
@@ -19,8 +19,8 @@ describe 'Olympian Stats` API' do
 
     expect(stats['total_competing_olympians']).to eq(3)
     expect(stats['average_weight']['unit']).to eq('kg')
-    expect(stats['average_weight']['male_olympians']).to eq("62.5")
-    expect(stats['average_weight']['female_olympians']).to eq("55.0")
-    expect(stats['average_age']).to eq("32.5")
+    expect(stats['average_weight']['male_olympians']).to eq('62.5')
+    expect(stats['average_weight']['female_olympians']).to eq('55.0')
+    expect(stats['average_age']).to eq('32.5')
   end
 end
