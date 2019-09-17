df31aad516ac51184b612b36b76d45a6df399fae
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-17 11:34:22 -0600

    Add sex to Olympians in OlympianStats spec, change values accordingly for expectations

--- spec/requests/api/v1/olympian_stats_spec.rb
+++ spec/requests/api/v1/olympian_stats_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'Olympian Stats` API' do
   it 'sends a response with stats for all stored Olympians' do
-    create(:olympian, name: 'Brian Plantico', age: 17, weight: 60)
-    create(:olympian, name: 'Brian Plantico', age: 17, weight: 60)
-    create(:olympian, name: 'Brian Plantico', age: 17, weight: 60)
-    create(:olympian, name: 'Alexandra Chakres', age: 55, weight: 55)
-    create(:olympian, name: 'Alexandra Chakres', age: 55, weight: 55)
-    create(:olympian, name: 'Earl Stephens', age: 34, weight: 70)
+    create(:olympian, name: 'Brian Plantico', age: 17, weight: 60, sex: 'M')
+    create(:olympian, name: 'Brian Plantico', age: 17, weight: 60, sex: 'M')
+    create(:olympian, name: 'Brian Plantico', age: 17, weight: 60, sex: 'M')
+    create(:olympian, name: 'Alexandra Chakres', age: 55, weight: 55, sex: 'F')
+    create(:olympian, name: 'Alexandra Chakres', age: 55, weight: 55, sex: 'F')
+    create(:olympian, name: 'Earl Stephens', age: 34, weight: 70, sex: 'M')
 
     get "/api/v1/olympian_stats"
 
@@ -17,8 +19,8 @@ describe 'Olympian Stats` API' do
 
     expect(stats['total_competing_olympians']).to eq(3)
     expect(stats['average_weight']['unit']).to eq('kg')
-    expect(stats['average_weight']['male_olympians']).to eq(65.0)
-    expect(stats['average_weight']['female_olympians']).to eq(55.0)
-    expect(stats['average_age']).to eq(35.3)
+    expect(stats['average_weight']['male_olympians']).to eq("62.5")
+    expect(stats['average_weight']['female_olympians']).to eq("55.0")
+    expect(stats['average_age']).to eq("32.5")
   end
 end
